78ddb4a5f2faebde5cfa1dc296b6c95056311aae
CrazinessJoy <crazinessjoy@gmail.com>
2026-02-06 17:43:49 -0600

    Add Pomodoro Timer code \nPomodoro timer has now been created -Brittney Situ 2/6

--- UnCram.py
+++ UnCram.py
@@ -1,3 +1,5 @@
+import time
+
 class Uncram:
     # This is the main class for the Uncram tool
     pass
@@ -15,6 +17,37 @@ class FocusModeTimer:
     # It breaks work into 25 minute "work" intervals with 5 minute breaks in between. 
     # After four "Pomodoros", the user takes a longer break of 15-30 minutes. Maybe we can let them set this one
 
+    def __init__(self, work_min=25, break_min=5):
+        self.work_sec = work_min * 60
+        self.break_sec = break_min * 60
+        self.reps = 0
+
+    def start_session(self, duration, label="Work"):
+        print(f"\n--- {label} Session Started ---")
+        while duration > 0:
+            mins, secs = divmod(duration, 60)
+            # \r returns the cursor to the start of the line
+            print(f"Time Remaining: {mins:02d}:{secs:02d}", end="\r")
+            time.sleep(1)
+            duration -= 1
+        print(f"\n{label} session complete!")
+
+    def run(self):
+        try:
+            while True:
+                self.start_session(self.work_sec, "Work")
+                self.reps += 1
+                print(f"Total sessions completed: {self.reps}")
+                
+                self.start_session(self.break_sec, "Break")
+        except KeyboardInterrupt:
+            print("\nTimer stopped. Great work today!")
+
+# --- Execution ---
+if __name__ == "__main__":
+    # You can now customize times easily when you create the object
+    timer = FocusModeTimer(work_min=25, break_min=5)
+    timer.run()
 
     pass
 
