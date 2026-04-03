c6241c1e54f973cff12c55f98e8c220c976c8139
mindyng07 <mindynguyen1611@gmail.com>
2026-04-03 02:59:11 -0500

    Update UX/UI
    Update UX/UI, add break button, add sound to notify when timer done

--- UnCram.py
+++ UnCram.py
@@ -1,4 +1,4 @@
-from nicegui import ui
+from nicegui import ui, app
 
 class Task:               
     def __init__(self, name, due_day):
@@ -91,7 +91,8 @@ class FocusModeTimer:
     # It breaks work into 25 minute "work" intervals with 5 minute breaks in between. 
     # After four "Pomodoros", the user takes a longer break of 15-30 minutes. Maybe we can let them set this one
 
-    def __init__(self, work_min=25, break_min=5, long_break_min=20):
+
+    def __init__(self, work_min=1, break_min=1, long_break_min=20):
         self.work_sec = work_min * 60
         self.break_sec = break_min * 60
         self.long_break_sec = long_break_min * 60
@@ -100,14 +101,17 @@ class FocusModeTimer:
         self.is_running = False
         self.mode = "Work"
         self.completed_pomodoros = 0 # Track for long breaks
+        self.alarm = ui.audio('sound/alarm.mp3').classes('hidden')
         
-        with ui.card().classes('w-64 items-center shadow-lg'):
-            self.label = ui.label(self.mode).classes('text-h6')
+        # Build the UI
+        with ui.card().classes('w-80 items-center shadow-lg'):
+            self.label = ui.label(self.mode).classes('text-h6 font-bold')
             self.timer_display = ui.label(self.format_time()).classes('text-h3 font-mono')
             
             with ui.row():
                 self.start_btn = ui.button('Start', on_click=self.start).props('elevated')
                 ui.button('Reset', on_click=self.reset, color='red-5').props('outline')
+                ui.button('Start Break', on_click=self.start_break_session, color='orange').props('round')
             
             # Counter display
             self.stats = ui.label(f'Pomodoros: {self.completed_pomodoros}').classes('text-caption')
@@ -131,10 +135,22 @@ class FocusModeTimer:
         self.update_ui()
         self.start_btn.text = 'Start'
 
+    def start_break_session(self):
+        if self.mode == "Work":
+            self.is_running = False
+            self.tick_timer.active = False
+            self.mode = "Break"
+            self.time_left = self.break_sec
+            self.update_ui()
+            ui.notify("Break time! Relax for a bit.")
+        else:
+            ui.notify("You are already on a break!")
+
     def tick(self):
         if self.time_left > 0:
             self.time_left -= 1
         else:
+            self.alarm.play()  # Play alarm sound when timer ends
             if self.mode == "Work":
                 self.completed_pomodoros += 1
                 if self.completed_pomodoros % 4 == 0:
@@ -152,14 +168,24 @@ class FocusModeTimer:
         
         self.update_ui()
 
+
     def update_ui(self):
         self.timer_display.text = self.format_time()
         self.label.text = self.mode
         self.stats.text = f'Pomodoros: {self.completed_pomodoros}'
+    
+    # Change card color based on mode
+        if self.mode == "Work":
+            self.timer_display.classes('text-blue-500', remove='text-orange-500')
+        else:
+            self.timer_display.classes('text-orange-500', remove='text-blue-500')
 
+  
 # --- MAIN EXECUTION (Outdent these!) ---
-ui.label('Uncram!').classes('text-h4 q-ma-md')
-FocusModeTimer()
+with ui.column().classes('absolute-center items-center'):
+    ui.label('Uncram!').classes('text-h2 q-mb-md font-bold') # Made it bigger/bolder
+    FocusModeTimer()
+
 
 
 class DistractionBlocker:
@@ -179,4 +205,8 @@ class AmbientFocusAid:
 
 
 TimeBlockingScheduler()
-ui.run()
+
+app.add_static_files('/sound', 'sound') 
+
+if __name__ in {"__main__", "__mp_main__"}:
+    ui.run()
