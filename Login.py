f57b1ef9aab1d8b1eca9a1a67639c02c96c89797
CrazinessJoy <crazinessjoy@gmail.com>
2026-04-10 12:48:43 -0500

    Test label -Brittney 4/10/2026 12:48 pm

--- Login.py
+++ Login.py
@@ -1,37 +1,5 @@
 from nicegui import ui
 
 class Login:
-<<<<<<< HEAD
-
-    credentials = {
-        "alice": "123",
-        "bob": "password",
-        "charlie": "abc",
-    }
-
-    def __init__(self):
-        ui.label('Log In or Create An Account').style('font-size: 24px; font-weight: bold;')
-        ui.label('This product is still under development and creating an account is not yet supported.').style('color: #FF0000; font-size: 16px; font-weight: bold;')
-        ui.label('Please log in using one of the demo accounts: alice, bob, or charlie')
-
-        self.username_input = ui.input("Username")
-        self.password_input = ui.input("Password", password=True)
-
-        ui.button("Login", on_click=self.try_login)
-
-    def try_login(self):
-        username = self.username_input.value
-        password = self.password_input.value
-
-        if username not in self.credentials or self.credentials[username] != password:
-            ui.notify("Username or password is incorrect")
-            return
-
-        ui.notify("Login successful")
-
-Login()
-ui.run()
-=======
     
     pass
->>>>>>> 662b757 (Initial commit -Brittney 4/10/2026 12:45 pm)
