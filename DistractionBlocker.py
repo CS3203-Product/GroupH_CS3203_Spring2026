abf132594897fededc27945a6bea4830b00b9003
Shrieu <shrieu@Vemolia.local>
2026-04-19 02:02:29 -0500

    Add distraction blocker feature with fastAPI routes and nicegui page

--- DistractionBlocker.py
+++ DistractionBlocker.py
@@ -23,7 +23,7 @@ class DistractionBlocker:
         total_minutes = hours * 60 + minutes
 
         # Task session (start and end time block): 10:30 (630 minutes) to 20:00 (1200 minutes)
-        session_start = 0
+        session_start = 10*60+30
 
         session_end = 24 * 60
 
