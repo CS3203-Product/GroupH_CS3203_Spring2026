550e5b1f76a46a372b2c43e51f5011ec9b9ccd37
Shrieu <shrieu@Vemolia.local>
2026-05-03 17:30:49 -0500

    Updated chrome extension to use the Render URL instead of localhost

--- DistractionBlocker_extension/background.js
+++ DistractionBlocker_extension/background.js
@@ -1,43 +1,36 @@
-
-// Triggers when user navigates to another website
 chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
-  
-  // Checks the main address bar and disregards ads, embedded videos
+
+// Checks the main address bar and disregards ads, embedded videos
   if (details.frameId !== 0) return;
 
-  // URL string converts to URL object for extracting domain name
+// URL string converts to URL object for extracting domain name
   const url = new URL(details.url);
-  
-  // Cleans domain: e.g., "://youtube.com" becomes "youtube.com"
-  const hostname = url.hostname.replace('www.', '');
 
-  // Ignores internal extension pages to prevent a nonstop indirect loop
-  if (url.protocol === 'chrome-extension:') return;
+// Cleans domain: e.g.,  “://youtube.com” becomes “youtube.com”
+  const hostname = url.hostname.replace('www.', '');
 
   try {
 
-    // Send the domain to your FASTAPI to check if domain is blocked
-    const response = await fetch('http://localhost:8000/blocker/check-url', {
+// Send POST request to FASTAPI backend hosted on Render. The fetch sends the current domain to a remote FASTAPI endpoint to check if the site is restricted based on user focus settings. Centralizes blocking logic for the backend so the extension makes real time decisions using records from Supabase. 
+    const response = await fetch('https://grouph-cs3203-spring2026-h2lk.onrender.com/blocker/check-url', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ 
-        url: hostname, 
-        user_id: '123' 
-      })
+      body: JSON.stringify({
+url: hostname, 
+user_id: '123'
+ })
     });
 
     const data = await response.json();
 
-    // If FASTAPI indicates site is blocked, redirect the user to "blocked.html" page
+// If FASTAPI indicates site is blocked, redirect the user to “blocked.html” page 
     if (data.blocked) {
-      chrome.tabs.update(details.tabId, { 
-        url: chrome.runtime.getURL(`blocked.html?site=${hostname}`) 
-      });
+      chrome.tabs.update(details.tabId, { url: chrome.runtime.getURL(`blocked.html?site=${hostname}`) 
+});
     }
   } catch (err) {
 
-    // Log errors to extension console ( e.g., if FASTAPI server is offline) 
+// Log errors to extension console ( e.g., if FASTAPI server is offline)
     console.error('Blocker check failed:', err);
   }
 });
-
