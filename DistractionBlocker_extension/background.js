72b9e47e18f2ffe05bb513ce75922f1611f0b06e
Joshua Okoro <joshuaojc@gmail.com>
2026-05-08 10:54:09 -0500

    refactor: remove unnecessary debug logging in background.js

--- DistractionBlocker_extension/background.js
+++ DistractionBlocker_extension/background.js
@@ -40,9 +40,7 @@ chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
   const apiBase = await getApiBaseUrl();
 
   console.log('API BASE ->', apiBase);
-  console.log('Token ->', token);
   console.log('Host name ->', hostname);
-  console.log('ENV ->', apiEnvironment);
 
   try {
     const response = await fetch(`${apiBase}/blocker/check-url`, {
